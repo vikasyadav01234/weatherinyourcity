5b90b2441bba015f989274154692cfbc3f43706b
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-01 11:45:22 +0530

    main

--- vikas.js
+++ vikas.js
@@ -1,5 +1,5 @@
-const userTab = document.querySelector("data-userWeather");
-const searchTab = document.querySelector("data-searchWeather");
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
 const userContainer = document.querySelector(".weather-container");
 const grantAccessContainer = document.querySelector(".grant-location-container");
 const searchForm = document.querySelector("[data-searchForm]");
@@ -18,15 +18,18 @@ function switchTab(clickedTab){
         currentTab.classList.remove("current-tab");
         currentTab = clickedTab;
         currentTab.classList.add("current-tab");
+
     }
 }
 
 userTab.addEventListener("click", () => {
     //pass clicked tab as input parameter
+    console.log("Clicked on usertab");
     switchTab(userTab);
 });
 
 searchTab.addEventListener("click", () =>{
     //pass clicked tab as input parameter
+    console.log("Clicked on search Tab")
     switchTab(searchTab);
 });
